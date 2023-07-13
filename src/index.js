d4703a12e1c7bd5af7403d325e0d43de0a529dd4
kris-panko <krispanko@gmail.com>
2023-07-13 16:39:02 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  console.log("DOM loaded Kris");
 });
+const form = document.getElementById("create-task-form").addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+const submitButton = document.getElementById("submitButton").addEventListener("click", () => createNewTask()); 
+
+const createNewTask = () => {
+  const formInput = document.getElementById("new-task-description")
+  console.log(formInput.value);
+  
+  const node = document.createElement("li");
+  const textNode = document.createTextNode(formInput.value);
+
+  node.appendChild(textNode);
+  document.getElementById("tasks").appendChild(node);
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "x";
+  deleteButton.addEventListener("click", () => {
+    node.remove();
+  });
+
+  node.appendChild(deleteButton);
+
+
+  
+}
